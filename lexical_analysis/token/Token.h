127306391e7f8a5d3a778b8f5905777315770c69
chatzakis <chatzakis@ics.forth.gr>
2021-03-01 20:18:28 +0200

    output fix

--- lexical_analysis/token/Token.h
+++ lexical_analysis/token/Token.h
@@ -1,30 +1,42 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
-enum OPERATOR
-{
-    PLUS,
-    PLUS_PLUS,
-    MINUS,
-    MINUS_MINUS
+enum TOKEN_TYPE {
+  UNDEFINED,
+  KEYWORD,
+  OPERATOR,
+  CONSTINT,
+  CONSTREAL,
+  STRING,
+  IDENT,
+  COMMENT,
+  PUNCTUATION
 };
 
-struct alpha_token_t
-{
-    unsigned int numline;              /* Arithmos Grammis */
-    unsigned int numToken;             /* Arithmos Token */
-    char *content;                     /* Periexomena Token*/
-    char *type;                        /* Eidos Token */
-    struct alpha_token_t *alpha_yylex; /* Next Token */
+// enum COMMENT_TYPE { LINE_COMMENT, BLOCK_COMMENT, NESTED_COMMENT };
+
+// enum OPERATOR_TYPE { PLUS, PLUS_PLUS, MINUS, MINUS_MINUS };
+
+struct alpha_token_t {
+  unsigned int numline;  /* Arithmos Grammis */
+  unsigned int endline;  /* Arithmos telous grammis */
+  unsigned int numToken; /* Arithmos Token */
+
+  char *content;              /* Periexomena Token*/
+  enum TOKEN_TYPE token_type; /* Typos Token */
+  char *type;                 /* Eidos Token */
+  void *type_val;
+  struct alpha_token_t *alpha_yylex; /* Next Token */
 };
 
 typedef struct alpha_token_t alpha_token_t;
 
 alpha_token_t *alpha_token_init();
 
-void alpha_token_insert(alpha_token_t *head, unsigned int numline, char *content, char *type);
+void alpha_token_insert(alpha_token_t *head, unsigned int numline,
+                        char *content, enum TOKEN_TYPE token_type, char *type);
 
 void alpha_token_print(alpha_token_t *token);
 
