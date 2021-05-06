f557eded73823e5dfd18fe2f07c5bb5f3399ad69
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-05-06 17:52:19 +0300

    Update script.py

--- build/script.py
+++ build/script.py
@@ -1,25 +1,53 @@
+#############################################################
+#                  Alpha Compiler Script                    #
+#                                                           #
+# This script runs all the test for Alpha Compiler,         #
+# if there exists a folder organized with folders           #
+# following the format seen in file harvesting.             #
+#                                                           #
+# Manos Chatzakis, Nikos Fanourakis                         #
+#############################################################
+
 import os
 import subprocess
 
 compilerName = "calc"
 
+rootDirectory = "./../tests"
+
 do1 = 0
-do2 = 0
+do2 = 1
 do3 = 0
+printTests = 1
 
 phase1_tests = []
 phase2_tests = []
 phase3_tests = []
 
-for path, subdirs, files in os.walk('./../tests/phase1/'):
+def printAllTests():
+    print("Tests run by the script:")
+    if do1:
+        for i in range(len(phase1_tests)):
+            print(phase1_tests[i])
+
+    if do2:
+        for i in range(len(phase2_tests)):
+            print(phase2_tests[i])
+
+    if do3:
+        for i in range(len(phase3_tests)):
+            print(phase3_tests[i])
+
+
+for path, subdirs, files in os.walk(rootDirectory + "/phase1/"):
     for name in files:
         phase1_tests.append(os.path.join(path, name))
 
-for path, subdirs, files in os.walk('./../tests/phase2/'):
+for path, subdirs, files in os.walk(rootDirectory + "/phase2/"):
     for name in files:
         phase2_tests.append(os.path.join(path, name))
 
-for path, subdirs, files in os.walk('./../tests/phase3/'):
+for path, subdirs, files in os.walk(rootDirectory + "/phase3/"):
     for name in files:
         phase3_tests.append(os.path.join(path, name))
 
@@ -69,3 +97,7 @@ command = "make clean"
 print(command)
 out = subprocess.getoutput(command)
 print(out)
+
+if printTests:
+    print("==========================================")
+    printAllTests()
