1f6ce32d154b722628d6862fe929669fdb0df85c
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-07-06 20:02:07 +0300

    tests cleanup

---
+++ test/basic_simple.js
@@ -0,0 +1,62 @@
+//OK
+a = b = 5;
+
+function f1(a,b)
+{
+	if(a > b) 
+		return true;
+	else	
+		return false;
+}
+
+function sum(i)
+{
+	sumV = 0;
+    j = 0;
+	
+	while(j < i and i<20)
+	{
+		sumV = sumV + j++;
+	}
+    
+	return sumV;
+}
+
+{
+    print("a2 = 2 + 2 * 2 / 8;\n");
+	a2 = 2 + 2 * 2 / 8;
+	print(a2);
+
+    print("c = f1(3,4);");
+	c = false;//f1(3,4);
+	print(c);
+
+	print("local a = (1>2 and ::b <= 6) or (c != false);\n");
+	local a = false;//(1>2 and ::b <= 6) or (c != false);
+    print(a);
+	
+
+	print("global b = not a;\n");
+	::b = not a;
+	print(b);
+	
+	if(a) {
+		// never here
+        print("The impossible happened\n");
+    }
+	else
+	{
+		i = 0;
+        res = 0;
+		
+		while((res <= 15))
+		{
+			res = sum(i);
+			print("sum: ",i," = ",res,"\n");
+			i++;
+		}
+	}
+	
+	c = 3 + -2 * -2;
+	print(c);
+}
---
+++ test/tables3.js
@@ -0,0 +1,6 @@
+//send by email: anikolul@csd.uoc.gr
+t = [10, 20, 30, 40];
+
+t["self"] = t;
+
+print(t);
